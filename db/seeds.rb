b8eb23dbb3ccbb8757c7da70454e190fe4aebd80
jhonnatas <jhonnatas.aires@gmail.com>
2023-01-22 22:03:17 -0300

    Feature/create Accounts using seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create!(email: 'jhonnatas.aires@gmail.com', password: '123456')
+#user = User.create!(email: 'jhonnatas.aires@gmail.com', password: '123456')
+
+5.times {
+  Account.create!(
+    account_number: Faker::Bank.account_number, 
+    bk_number: Faker::Bank.bsb_number, 
+    bk_branch: Faker::Bank.name,
+    details: Faker::Lorem.sentences,
+    user_id: 1,
+    account_active: true,
+    balance: 100.0)
+}
+
+
